9c3f5fb0dc94e60daf2c024fec0eda724eeab9fa
ydy <yudingyuan@aliyun.com>
2021-01-21 11:36:05 +0800

    ed: cross-env

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_SOCKET: '"http://10.20.20.24:9090/"'
+  VUE_APP_SOCKET:
+    '"http://' + process.env.VUE_APP_SOCKET + '/"' ||
+    '"http://10.20.20.24:9090/"'
 });
