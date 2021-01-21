d1cd4bd6b007f4aaa046c37d89017d53085f9dfe
ydy <yudingyuan@aliyun.com>
2021-01-21 11:37:19 +0800

    ed: webpack

--- config/dev-other.env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const merge = require("webpack-merge");
-const prodEnv = require("./prod.env");
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_SOCKET: '"http://10.20.20.125:9090/"'
-});
